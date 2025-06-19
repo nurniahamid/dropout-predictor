f03ad84422207b1d70cba7ae3d636cb4e9a4f18f
nurniahamid <nurniahamid103@student.unsrat.ac.id>
2025-06-19 17:30:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,6 @@ st.markdown("Masukkan data mahasiswa untuk memprediksi kemungkinan dropout:")
 # Form input dari pengguna
 gender = st.selectbox("Jenis Kelamin", ["Laki-laki", "Perempuan"])
 age = st.number_input("Usia Saat Mendaftar", min_value=15, max_value=60, value=18)
-admission_grade = st.number_input("Nilai Masuk (Admission Grade)", min_value=0.0, max_value=200.0, value=140.0)
 semester1 = st.number_input("Nilai Semester 1", min_value=0.0, max_value=20.0, value=12.0)
 semester2 = st.number_input("Nilai Semester 2", min_value=0.0, max_value=20.0, value=13.0)
 
